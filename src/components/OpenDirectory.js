ed547b87e5a5ac0f305a5a4e3c3b7587dc559d8d
Reece Mercer <reecemercer981@gmail.com>
2019-11-16 01:17:09 +0000

    Added checkboxes. Need to link to state

--- src/components/OpenDirectory.js
+++ src/components/OpenDirectory.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 import Search from '@material-ui/icons/Search';
+import { Container } from '@material-ui/core';
 
 class OpenDirectory extends React.Component {
 	constructor(props) {
@@ -13,7 +17,10 @@ class OpenDirectory extends React.Component {
 		
 		this.state = {
 			searchTerm: "",
-			uri: `intext:"" intitle:"index.of" -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml)`
+			uri: `intext:"" intitle:"index.of" -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml)`,
+			everything: true,
+			audio: false,
+			video: false
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -32,7 +39,7 @@ class OpenDirectory extends React.Component {
 				<Card>
 					<CardContent>
 						<Typography className="Title" color="textPrimary" gutterBottom>
-							Open Directory search tool {this.state.uri}
+							Open directory search tool
 						</Typography>
 						<form noValidate autoComplete="off">
 							<div>
@@ -44,15 +51,34 @@ class OpenDirectory extends React.Component {
 									margin="normal"
 								/>
 							</div>
+								<FormGroup aria-label="position" row>
+									<Container maxWidth="sm" className="CenteredContainer">
+										<FormControlLabel
+											value="start"
+											control={<Checkbox color="primary" />}
+											label="Audio"
+											labelPlacement="start"
+										/>
+										<FormControlLabel
+											value="start"
+											control={<Checkbox color="primary" />}
+											label="Video"
+											labelPlacement="start"
+										/>
+									</Container>
+								</FormGroup>
 						</form>
-						<a href={`https://www.google.com/search?q=${this.state.uri}`} target="_blank">
+						<a href={`https://www.google.com/search?q=${this.state.uri}`} 
+						target="_blank"
+						rel="noopener noreferrer"
+						>
 						<Button
               variant="contained"
 							color="primary"
 							startIcon={<Search />}
               style={{margin: "auto"}}
             >
-              Find Directories!
+						Find Directories
             </Button>
 						</a>
 					</CardContent>
